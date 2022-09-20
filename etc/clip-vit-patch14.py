6e6370bc0540eec764cf2d51a08bed0777022cc5
Sambhav Saxena <76242518+sambhavsaxena@users.noreply.github.com>
2022-09-20 11:23:53 +0530

    created `clip-vit-patch14.py` etc config module

---
+++ etc/clip-vit-patch14.py
@@ -0,0 +1,31 @@
+#text generation using Amazon Sagemaker engine using clip-vit
+
+from sagemaker.huggingface import HuggingFaceModel
+import boto3
+
+iam_client = boto3.client('iam')
+role = iam_client.get_role(RoleName='{IAM_ROLE_WITH_SAGEMAKER_PERMISSIONS}')['Role']['Arn']
+# Hub Model configuration. https://huggingface.co/models
+hub = {
+	'HF_MODEL_ID':'openai/clip-vit-large-patch14',
+	'HF_TASK':'text-generation'
+}
+
+# create Hugging Face Model Class
+huggingface_model = HuggingFaceModel(
+	transformers_version='4.17.0',
+	pytorch_version='1.10.2',
+	py_version='py38',
+	env=hub,
+	role=role, 
+)
+
+# deploy model to SageMaker Inference
+predictor = huggingface_model.deploy(
+	initial_instance_count=1, # number of instances
+	instance_type='ml.m5.xlarge' # ec2 instance type
+)
+
+predictor.predict({
+	'inputs': No input example has been defined for this model task.
+})
