d77c8a6cb3b9bd8b97b88130dda60a21cabe1c88
Sambhav Saxena <76242518+sambhavsaxena@users.noreply.github.com>
2022-10-20 17:25:35 +0530

    removed unnecessary token

--- app.py
+++ app.py
@@ -4,23 +4,18 @@ from flask import Flask, render_template, url_for
 from flask import request as req
 
 app = Flask(__name__)
-
-
+app.config['KEY'] = environ.get('KEY')
 @app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("index.html")
-
-
 @app.route("/summary", methods=["GET", "POST"])
 def summary():
     if req.method == "POST":
         API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
-        headers = {
-            "Authorization": f"Bearer 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"}
+        headers = {"Authorization": f"Bearer " + app.config['KEY']}
         data = req.form["data"]
         maxlength = int(req.form["maxlength"])
         minlength = maxlength//4
-
         def query(payload):
             response = requests.post(API_URL, headers=headers, json=payload)
             return response.json()
@@ -31,7 +26,6 @@ def summary():
         return render_template("index.html", result=output["summary_text"])
     else:
         return render_template("index.html")
-
 
 if __name__ == "__main__":
     app.run()
